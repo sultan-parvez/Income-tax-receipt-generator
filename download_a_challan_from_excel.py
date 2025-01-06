c2f3c6ab6a304255684be5ab75610ebeea0b46c0
sultan-parvez <sultan.parvez@dsinnovators.com>
2025-01-06 17:33:06 +0600

    Updated Chrome Settings

--- download_a_challan_from_excel.py
+++ download_a_challan_from_excel.py
@@ -1,44 +1,35 @@
 import time
 import pytest
 import pandas as pd
-import pyautogui
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
+import base64
+import os
 
 # Load test data from an Excel file
 def load_data_from_excel():
     df = pd.read_excel("data.xlsx")  # Replace with your Excel file path
     return df.to_dict(orient="records")  # Convert the DataFrame to a list of dictionaries
 
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--ignore-certificate-errors")  # Ignore SSL certificate errors
+chrome_options.add_argument("--headless=new")  # Headless mode for running without UI
+
 @pytest.mark.parametrize("data", load_data_from_excel())
 def test_download_a_challan(data):
-    # Configure Firefox options
-    firefox_options = Options()
-    firefox_options.headless = True  # Run in headless mode
 
-    firefox_options.set_preference("print.always_print_silent", True)  # Silent printing
-    firefox_options.set_preference("print.show_print_progress", False)
-    firefox_options.set_preference("print.printer_Mozilla_Save_to_PDF.print_to_file", True)
-    firefox_options.set_preference("print.printer_Mozilla_Save_to_PDF.print_to_filename", "output.pdf")
-    firefox_options.set_preference("print.printer_Mozilla_Save_to_PDF.print_paper_name", "iso_a4")
-    firefox_options.set_preference("print.printer_Mozilla_Save_to_PDF.print_to_file", True)
-
-    # Disable headers and footers
-    firefox_options.set_preference("print.print_headerleft", "")  # No left header
-    firefox_options.set_preference("print.print_headercenter", "")  # No center header
-    firefox_options.set_preference("print.print_headerright", "")  # No right header
-    firefox_options.set_preference("print.print_footerleft", "")  # No left footer
-    firefox_options.set_preference("print.print_footercenter", "")  # No center footer
-    firefox_options.set_preference("print.print_footerright", "")  # No right footer
-
-    driver = webdriver.Firefox(options=firefox_options)
-
-    driver.implicitly_wait(5)
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)  
     driver.get("https://ibas.finance.gov.bd/acs/general/sales#/home/dashboard/")
 
     dropdown = driver.find_element(By.XPATH, "//div[@class='dropbtn']//div[contains(text(), 'এনবিআর এর জমা')]")
     dropdown.click()
+
+    time.sleep(2)
 
     challan_link = driver.find_element(By.CSS_SELECTOR, "a[data-challan-type-name-bn='কোম্পানিসমূহ কর্তৃক দেয় আয়কর']")
     challan_link.click()
@@ -46,6 +37,7 @@ def test_download_a_challan(data):
 
     dropdown2 = driver.find_element(By.XPATH, "//span[@id='select2-ddlIncomeTaxYear-container']")
     dropdown2.click()
+    driver.implicitly_wait(10)
 
     tin_input = driver.find_element(By.XPATH, "//input[@role='textbox']")
     tin_input.send_keys(2025)
@@ -53,20 +45,27 @@ def test_download_a_challan(data):
 
     input_tax_year = driver.find_element(By.CSS_SELECTOR, "span[class='select2-results']")
     input_tax_year.click()
+    driver.implicitly_wait(10)
 
     dropdown3 = driver.find_element(By.CSS_SELECTOR, "#select2-ddlIncomeTaxType-container")
     dropdown3.click()
+    driver.implicitly_wait(10)
 
     dropdown3Input = driver.find_element(By.XPATH,
                                          "//li[contains(text(), 'কোম্পানিসমূহ কর্তৃক দেয় আয়কর (পুরাতন কোড -0101)')]")
     dropdown3Input.click()
+    driver.implicitly_wait(10)
 
     dropdown4 = driver.find_element(By.CSS_SELECTOR,
                                     "span[id='select2-ddlNBRLawType-container'] span[class='select2-selection__placeholder']")
     dropdown4.click()
+    driver.implicitly_wait(10)
 
     dropdown4Input = driver.find_element(By.XPATH, "//span//ul//li[contains(text(), 'উৎসে কর (Source Tax)')]")
     dropdown4Input.click()
+    driver.implicitly_wait(10)
+
+    time.sleep(2)
 
     amount_inputs = driver.find_elements(By.CSS_SELECTOR,
                                          "table[id='IncomeTaxAccountsGrid'] td input[data-modelproperty='AMOUNT']")
@@ -79,81 +78,98 @@ def test_download_a_challan(data):
     dropdown5 = driver.find_element(By.CSS_SELECTOR,
                                     "span[class='select2-selection__rendered'][id='select2-ddlIncomeTaxLaw-container']")
     dropdown5.click()
+    driver.implicitly_wait(10)
 
     dropdown5Input = driver.find_element(By.XPATH,
                                          "//li[contains(text(), '৮৬-বেতন হতে উৎস কর (Deduction from Salary)')]")
     dropdown5Input.click()
+    driver.implicitly_wait(10)
 
     submit_button = driver.find_element(By.XPATH, "//button[@id='btnIncomeTaxOk']")
     submit_button.click()
+    driver.implicitly_wait(10)
 
     # Tax_Office Info
     tax_area = driver.find_element(By.CSS_SELECTOR, "span[id='select2-tax-zone-auto-suggest-container']")
     tax_area.click()
+    driver.implicitly_wait(10)
 
     tax_area_input = driver.find_element(By.XPATH, "//span//ul//li[contains(text(), '1110215102431-কর অঞ্চল-১৫, ঢাকা')]")
     tax_area_input.click()
+    driver.implicitly_wait(10)
 
     govt_office = driver.find_element(By.CSS_SELECTOR, "span[id='select2-organization-auto-suggest-container']")
     govt_office.click()
+    driver.implicitly_wait(10)
 
     govt_office_input = driver.find_element(By.XPATH, "//span//ul//li[contains(text(), 'ঢাকা-Circle-316')]")
     govt_office_input.click()
+    driver.implicitly_wait(10)
 
     industry_button = driver.find_element(By.CSS_SELECTOR, '#btnPrivateWithETIN')
     industry_button.click()
+    driver.implicitly_wait(10)
 
     individual_tin = driver.find_element(By.CSS_SELECTOR, '#txtNonGovTIN')
     individual_tin.send_keys(data['TIN'])
+    driver.implicitly_wait(10)
 
     individual_tin_check = driver.find_element(By.CSS_SELECTOR, '#btnChkTIN')
     individual_tin_check.click()
+    driver.implicitly_wait(10)
 
     select_industry_tin = driver.find_element(By.XPATH, "//div[@id='rdoPersonIdentificationTypeContainer']//span[@class='form-radio-sign'][contains(text(),'টিআইএন')]")
     select_industry_tin.click()
+    driver.implicitly_wait(10)
 
     industry_tin = driver.find_element(By.CSS_SELECTOR, '#txtBelow18BR')
     industry_tin.send_keys(592944949181)
+    driver.implicitly_wait(10)
 
     industry_tin_check = driver.find_element(By.CSS_SELECTOR, '#btnChkIdentificaionNumber')
     industry_tin_check.click()
+    driver.implicitly_wait(10)
 
     industry_mobile = driver.find_element(By.CSS_SELECTOR, '#txtMobile')
     industry_mobile.send_keys("01730037400")
+    driver.implicitly_wait(10)
 
     select_bank_option = driver.find_element(By.CSS_SELECTOR, "label[for='depositInBankCounter']")
     select_bank_option.click()
+    driver.implicitly_wait(10)
 
     comment_box = driver.find_element(By.CSS_SELECTOR, '#txtRemarks')
     comment_box.send_keys(data["COMMENT"])
+    driver.implicitly_wait(10)
 
     final_submit_button = driver.find_element(By.CSS_SELECTOR, '#btnSave')
     final_submit_button.click()
+    driver.implicitly_wait(10)
 
     confirm_box = driver.find_element(By.CSS_SELECTOR, "#btnMessageOK")
     confirm_box.click()
+    driver.implicitly_wait(10)
 
     window_before = driver.window_handles[0]
     window_after = driver.window_handles[1]
     driver.switch_to.window(window_after)
+    time.sleep(3)
 
-    # Use the DevTools Protocol to print the page as a PDF
     print_settings = {
-        "paperWidth": 8.5,
-        "paperHeight": 11.0,
-        "printBackground": True
+        "paperWidth": 8.5,  # Width of the paper in inches
+        "paperHeight": 11.0,  # Height of the paper in inches
+        "printBackground": True,  # Include background graphics
+        "scale": 0.80  # Scale the content to 80%
     }
-    driver.execute_script("window.print();")
+
+    result = driver.execute_cdp_cmd("Page.printToPDF", print_settings)
+    output_dir = "a_challans"
+    os.makedirs(output_dir, exist_ok=True)
+    output_file = os.path.join(output_dir, f"{data['NAME']}{data['ID']}.pdf")
+    with open(output_file, "wb") as f:
+        f.write(base64.b64decode(result["data"]))
+
+    print(f"PDF saved as {output_file}")
    
-
-    # Allow the print dialog to open and interact with it
-    time.sleep(2)  # Adjust the time as needed for the dialog to appear
-
-    # Type the file name in the "Save As" dialog
-    pyautogui.write(str(data["NAME"]) + "_"+ str(data["ID"]) + ".pdf")
-
-    # Press Enter to confirm the save
-    pyautogui.press("enter")
-
     driver.switch_to.window(window_before)
     driver.quit()
